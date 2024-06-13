3b7f29eb858ed5bf3ccac02596aa2aca80f9c738
Alfredo Miranda <alfredocdmiranda@gmail.com>
2024-06-13 20:08:59 +0100

    feat: Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+VERSION = open("VERSION").read()
+
+setup(
+    name='imgrott',
+    version=VERSION,
+    description='Proxy Growatt Monitor Server',
+    url='https://github.com/alfredocdmiranda/imgrott',
+    author='Alfredo Miranda',
+    author_email='alfredocdmiranda@gmail.com',
+    license='',
+    packages=['imgrott'],
+    entry_points={
+        'console_scripts': ['imgrott=imgrott.main:main'],
+    }
+)
